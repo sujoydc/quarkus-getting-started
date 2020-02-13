11a8667685a62c819e7adff5b1eed3a59709ee3b
Sujoy Datta Choudhury <sujoydc@gmail.com>
2020-02-13 07:25:15 -0500

    This is my first commit

---
+++ src/main/java/org/acme/quickstart/GreetingResource.java
@@ -0,0 +1,17 @@
+package org.acme.quickstart;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/hello")
+public class GreetingResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response hello() {
+        return "hello";
+    }
+}
